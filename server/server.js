e4d0396f3aa9e90e0a8443c0c40ce5bb920c274a
cameron.morrongiello <35345985+cameron-morrongiello@users.noreply.github.com>
2020-12-08 14:52:03 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const config = require('../config/config.js');
+const config = require('./config/config.js');
 const morgan = require('morgan');
-const Router = require('./Router.js');
+const Router = require('./server/Router.js');
 const app = express();
 const cors = require('cors');
 const path = require('path');
@@ -46,13 +46,13 @@ require('../config/passport.js')(passport);
 app.use('/', Router);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-app.listen(process.env.PORT || 5000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
